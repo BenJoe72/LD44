a0b98e444a417f682ea75584a170750c08ac1c26
BenJoe72 <benjoe93@gmail.com>
2019-04-27 20:53:48 +0200

    more stuff I guess

--- Assets/Scripts/Player/InteractScript.cs
+++ Assets/Scripts/Player/InteractScript.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class InteractScript : MonoBehaviour
 {
+    private bool _onBloodPlace;
+    private bool _onPlasmaPlace;
+    private bool _onShopPlace;
+    private bool _onUpgradePlace;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact"))
+        {
+
+        }
+    }
+
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("MoneyPickup"))
@@ -16,18 +30,22 @@ public class InteractScript : MonoBehaviour
         }
         else if (other.tag.Equals("BloodPlace"))
         {
-            Debug.Log("You are standing on the bloodplace!");
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = true;
+            Debug.Log("You are standing on the bloodspace!");
         }
         else if (other.tag.Equals("PlasmaPlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = true;
             Debug.Log("You are standing on the plasmapalce!");
         }
         else if (other.tag.Equals("ShopPlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = true;
             Debug.Log("You are standing on the shopplace!");
         }
         else if (other.tag.Equals("UpgradePlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = true;
             Debug.Log("You are standing on the upgradeplace!");
         }
     }
@@ -36,18 +54,22 @@ public class InteractScript : MonoBehaviour
     {
         if (other.tag.Equals("BloodPlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = false;
             Debug.Log("Exited the bloodplace!");
         }
         else if (other.tag.Equals("PlasmaPlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = false;
             Debug.Log("Exited the plasmapalce!");
         }
         else if (other.tag.Equals("ShopPlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = false;
             Debug.Log("Exited the shopplace!");
         }
         else if (other.tag.Equals("UpgradePlace"))
         {
+            other.transform.Find("Prompt").GetComponent<SpriteRenderer>().enabled = false;
             Debug.Log("Exited the upgradeplace!");
         }
     }
