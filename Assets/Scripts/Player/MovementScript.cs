ca651825beb789cf145274d246f61ed1dd1fea8e
BenJoe72 <benjoe93@gmail.com>
2019-04-27 13:04:15 +0200

    player movement and camera

--- Assets/Scripts/Player/MovementScript.cs
+++ Assets/Scripts/Player/MovementScript.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    public float RunSpeed = 2f;
+    public Camera mainCamera;
+
+    private Vector3 _movementVector;
+    private Vector3 _mousePosition;
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _movementVector = Vector3.zero;
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        GetMovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Rotate();
+    }
     
+    private void GetMovementInput()
+    {
+        _movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    private void Move()
+    {
+        var forward = mainCamera.transform.TransformDirection(_movementVector.normalized);
+        forward = new Vector3(forward.x, 0, forward.z);
+        _mousePosition = transform.position + forward;
+        _rigidbody.MovePosition(transform.position + forward * (RunSpeed) / 10f);
+        _animator.SetBool("Run", forward != Vector3.zero);
+    }
+
+    private void Rotate()
+    {
+        transform.LookAt(_mousePosition);
+    }
 }
